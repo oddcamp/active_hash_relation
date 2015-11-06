2e53be58824050dbf1495a5c4383e0602f629661
Filippos Vasilakis <vasilakisfil@gmail.com>
2015-11-06 09:47:16 +0100

    fix minor bug with symbols on sorting

--- lib/active_hash_relation/sort_filters.rb
+++ lib/active_hash_relation/sort_filters.rb
@@ -1,6 +1,6 @@
 module ActiveHashRelation::SortFilters
   def apply_sort(resource, params, model = nil)
-    if model.columns.map(&:name).include?(params[:property])
+    if model.columns.map(&:name).include?(params[:property].to_s)
       resource = resource.order(params[:property] => (params[:order] || :desc) )
     end
 
